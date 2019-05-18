3607853bb1bee75771365f9ff06ebaa1ad1fa9f3
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-05-18 16:48:09 +0900

    GPSで緯度経度を画面表示(「nyanのアプリ開発」からコピペ) + ログも画面表示

--- app/src/main/java/com/example/locationapp01/MainActivity.java
+++ app/src/main/java/com/example/locationapp01/MainActivity.java
@@ -1,13 +1,150 @@
 package com.example.locationapp01;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+    TextView textView3;
+    String debugLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView3 = findViewById(R.id.textView3);
+        debugLog = new String("");
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},
+                    1000);
+        }
+        else{
+            locationStart();
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 50, this);
+
+        }
+    }
+
+    private void logger(String log) {
+        debugLog += log;
+        textView3.setText(debugLog);
+    }
+
+    private void locationStart() {
+        Log.d("LLLL","locationStart()");
+        logger("locationStart()");
+
+        // LocationManager インスタンス生成
+        locationManager =
+                (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("LLLL", "location manager Enabled");
+            logger("location manager Enabled");
+        } else {
+            // GPSを設定するように促す
+            Intent settingsIntent =
+                    new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("LLLL", "not gpsEnable, startActivity");
+            logger("not gpsEnable, startActivity");
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+
+            Log.d("LLLL", "checkSelfPermission false");
+            logger("checkSelfPermission false");
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 50, this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[]permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("LLLL","checkSelfPermission true");
+                logger("checkSelfPermission true");
+
+                locationStart();
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    // 以下、LocationListenerの必須Override
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.d("LLLL", "LocationProvider.AVAILABLE");
+                logger("LocationProvider.AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.d("LLLL", "LocationProvider.OUT_OF_SERVICE");
+                logger("LocationProvider.OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.d("LLLL", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                logger("LocationProvider.TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // 緯度の表示
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        String str1 = "Latitude:"+location.getLatitude();
+        textView1.setText(str1);
+
+        // 経度の表示
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        String str2 = "Longtude:"+location.getLongitude();
+        textView2.setText(str2);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
     }
 }
